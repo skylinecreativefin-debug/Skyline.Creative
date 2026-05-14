94f2563faa90483e44b4a0df428199dd8158f31a
skylinecreativefin-debug <skyline.creative.fin@gmail.com>
2026-05-15 00:12:35 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const nav = document.querySelector("nav");
-
-if(menuBtn){
-  menuBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
-}
