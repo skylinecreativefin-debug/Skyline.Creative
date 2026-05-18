4abf279b30b12ae9cc411058e8731790b75f94cf
skylinecreativefin-debug <skyline.creative.fin@gmail.com>
2026-05-18 06:07:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const nav = document.querySelector("nav");
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+const hiddenElements = document.querySelectorAll(".card, .pricing-card, .about-box, .contact-box");
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+
+  });
+
+});
+
+hiddenElements.forEach((el) => {
+  el.classList.add("hidden");
+  observer.observe(el);
+});
